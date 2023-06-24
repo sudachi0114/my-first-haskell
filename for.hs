7cce31c0083919b31a9ca66eaa9a48e42c129dd1
sudachi0114 <jnevariste@gmail.com>
2023-06-24 20:17:33 +0900

    for 文を使って数字のリストの要素の総和を求める関数を定義

--- for.hs
+++ for.hs
@@ -4,6 +4,21 @@ import Data.IORef    -- 変数を使う時に必要
 main = do
     printList [1..5]
 
+    {-
+        $ echo '6+7+8+9+10' | bc
+        40
+    -}
+    s <- getSum [6..10]
+    print s
+
 printList ls = do
     forM_ ls $ \i -> do
         print i
+
+getSum ls = do
+    s <- newIORef 0
+    forM_ ls $ \i -> do
+        c <- readIORef s
+        writeIORef s (c + i)
+    ret <- readIORef s
+    return ret
