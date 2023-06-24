9849fdda6e87d12a8592344579d0172bc540d6bb
sudachi0114 <jnevariste@gmail.com>
2023-06-24 20:27:04 +0900

    ユーザーの入力に対して返事をする、純粋でない関数内で if を使う

--- if.hs
+++ if.hs
@@ -1,4 +1,13 @@
 main = do
     putStrLn "挨拶といえば？:"
-    geeting <- getLine
-    putStrLn geeting
+    greeting <- getLine
+    answerToGreeting greeting
+
+-- 純粋でない関数内の `if`
+-- `then`, `else` の後に do をつける
+answerToGreeting greeting = do
+    if greeting == "Hi"
+        then do
+            putStrLn "English, Ok"
+        else do
+            putStrLn "英語でたのんます"
