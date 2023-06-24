e45e44bfae9a5ea6aca1cdc7cb3eb66c2550159c
sudachi0114 <jnevariste@gmail.com>
2023-06-24 20:10:29 +0900

    .. の挙動がよくわからなかったので遊んでみた

--- list.hs
+++ list.hs
@@ -7,8 +7,25 @@ main = do
     let ls = [1..4]  -- `..` は数字のリスト用の特殊な記法、間の数字を補ってくれる
     print ls          -- [1, 2, 3, 4]
 
+    -----
     let ls2 = [1,3..8]
     print ls2         -- [1, 3, 5, 7] ?!
+
+    let ls21 = [1,2..9]
+    print ls21        -- [1,2,3,4,5,6,7,8,9]
+
+    let ls22 = [1,3..9]
+    print ls22        -- [1,3,5,7,9]
+
+    let ls23 = [1,5..9]
+    print ls23        -- [1,5,9]
+
+    let ls24 = [1,6..9]
+    print ls24        -- [1,6]
+
+    let ls24 = [1,6..12]
+    print ls24        -- [1,6,11]
+    -----
 
     let ls3 = ["foo", "bar", "baz"]
     print ls3         -- ["foo", "bar", "baz"]
