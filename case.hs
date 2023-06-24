779ca279eed5719308be723f96d1135bb0899c8e
sudachi0114 <jnevariste@gmail.com>
2023-06-24 20:39:35 +0900

    純粋な関数の中で case を使う

--- case.hs
+++ case.hs
@@ -2,6 +2,7 @@ main = do
     putStrLn "number:"
     num <- getLine
     printInEnglish num
+    putStrLn (numInEnglish num)
 
 -- 純粋でない関数
 printInEnglish num = do
@@ -14,3 +15,11 @@ printInEnglish num = do
             putStrLn "three"
         _ -> do
             putStrLn "I don't know."
+
+-- 純粋な関数
+numInEnglish num =
+    case num of
+        "1" -> "one"
+        "2" -> "two"
+        "3" -> "three"
+        _ -> "わからん (´・ω・｀)"
