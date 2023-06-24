3e7fec62153bdcacca83c65d1459f375d0b73e39
sudachi0114 <jnevariste@gmail.com>
2023-06-24 20:29:56 +0900

    純粋な関数内で if を使う

--- if.hs
+++ if.hs
@@ -1,7 +1,15 @@
+{-
+if は純粋な関数の中でも使うことができる
+-}
+
 main = do
     putStrLn "挨拶といえば？:"
     greeting <- getLine
     answerToGreeting greeting
+
+    putStrLn "数字を入力してください:"
+    num <- getLine
+    putStrLn (checkNum num)
 
 -- 純粋でない関数内の `if`
 -- `then`, `else` の後に do をつける
@@ -11,3 +19,10 @@ answerToGreeting greeting = do
             putStrLn "English, Ok"
         else do
             putStrLn "英語でたのんます"
+
+-- 純粋な関数ないの `if`
+-- `then`, `else` の後に do がつかない
+checkNum num = 
+    if num == "0"
+        then "ゼロ〜"
+        else "ゼロ以外"
